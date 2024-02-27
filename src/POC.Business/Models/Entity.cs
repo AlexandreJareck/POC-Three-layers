d8e0dd1eaddbaff71228f51d291c68f12ca75473
AlexandreJareck <alexandrejareck@gmail.com>
2024-02-27 20:11:26 -0300

    feat: add base class (models)

---
+++ src/POC.Business/Models/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace POC.Business.Models
+{
+    public abstract class Entity
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
