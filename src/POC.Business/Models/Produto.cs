9468a23a5fe222e1c033731900fb7a17e92b5807
AlexandreJareck <alexandrejareck@gmail.com>
2024-02-27 20:23:00 -0300

    feat: modelando entidades

---
+++ src/POC.Business/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace POC.Business.Models
+{
+    public class Produto : Entity
+    {
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public decimal? Valor { get; set; }
+        public DateTime DataCadatro { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
