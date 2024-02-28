4e0047ca476b9199f1363e28e1547edc421070fa
AlexandreJareck <alexandrejareck@gmail.com>
2024-02-28 20:40:00 -0300

    feat: adicionad interfaces de repository

---
+++ src/POC.Business/Interfaces/IFornecedorRepository.cs
@@ -0,0 +1,13 @@
+﻿using POC.Business.Models;
+
+namespace POC.Business.Interfaces
+{
+    public interface IFornecedorRepository : IRepository<Fornecedor>
+    {
+        Task<Fornecedor> ObterFornecedorEndereco(Guid id);
+
+        Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id);
+
+        Task<Fornecedor> ObterEnderecoPorFornecedor(Guid fornecedorId);
+    }
+}
---
+++ src/POC.Business/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,13 @@
+﻿using POC.Business.Models;
+
+namespace POC.Business.Interfaces
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorId);
+
+        Task<IEnumerable<Produto>> ObterProdutosFornecedores();
+
+        Task<Produto> ObterProdutoFornecedor(Guid id);
+    }
+}
