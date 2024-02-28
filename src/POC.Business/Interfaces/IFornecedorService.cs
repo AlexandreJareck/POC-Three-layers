39aeb55b37b217e5614d1255c25fc44df7307891
AlexandreJareck <alexandrejareck@gmail.com>
2024-02-28 20:13:42 -0300

    feat: adiciona interfaces de serviços

---
+++ src/POC.Business/Interfaces/IFornecedorService.cs
@@ -0,0 +1,13 @@
+﻿using POC.Business.Models;
+
+namespace POC.Business.Interfaces
+{
+    public interface IFornecedorService : IDisposable
+    {
+        Task Adicionar(Fornecedor fornecedor);
+
+        Task Atualizar(Fornecedor fornecedor);
+
+        Task Remover(Guid id);
+    }
+}
---
+++ src/POC.Business/Interfaces/IProdutoService.cs
@@ -0,0 +1,13 @@
+﻿using POC.Business.Models;
+
+namespace POC.Business.Interfaces
+{
+    public interface IProdutoService : IDisposable
+    {
+        Task Adicionar(Produto produto);
+
+        Task Atualizar(Produto produto);
+
+        Task Remover(Guid id);
+    }
+}
