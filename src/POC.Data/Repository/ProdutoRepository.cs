d363075b221ffb6bb8d681458d4f1440390e4296
AlexandreJareck <alexandrejareck@gmail.com>
2024-03-06 23:30:46 -0300

    feat: adiciona repositorios

---
+++ src/POC.Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POC.Business.Interfaces;
+using POC.Business.Models;
+using POC.Data.Context;
+
+namespace POC.Data.Repository
+{
+    public class ProdutoRepository : Repository<Produto>, IProdutoRepository
+    {
+        public ProdutoRepository(MeuDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Produto> ObterProdutoFornecedor(Guid id)
+        {
+            return await Db.Produtos.AsNoTracking()
+                .Include(f => f.Fornecedor)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosFornecedores()
+        {
+            return await Db.Produtos.AsNoTracking()
+                .Include(f => f.Fornecedor)
+                .OrderBy(p => p.Nome)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorId)
+        {
+            return await Buscar(p => p.FornecedorId == fornecedorId);
+        }
+    }
+}
